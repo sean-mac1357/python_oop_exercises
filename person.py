d26122c0dbb062e5fd31deddd8bfdf39f2ce1bdb
Sean R MacEachern <sean.r.maceachern@gmail.com>
2020-12-15 11:34:06 -0500

    they greeted eachother

--- person.py
+++ person.py
@@ -7,10 +7,12 @@ class Person:
     def greet(self, other_person):
         print('Hello {}, I am {}!'.format(other_person.name, self.name))
 
-class = Vehicle
 
 sonny = Person("Sonny", "sonny@hotmail.com", "483-485-4948")
 print(sonny.name, sonny.email, sonny.phone)
 
 jordan = Person("Jordan", "jordan@aol.com", "495-586-3456")
 print(jordan.name, jordan.email, jordan.phone)
+
+sonny.greet(jordan)
+print(sonny.name, sonny.email, sonny.phone)
