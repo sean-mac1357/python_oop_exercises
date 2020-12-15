54b6344ea7fffd9d7b8bbc9be4dfb8262df682b4
Sean R MacEachern <sean.r.maceachern@gmail.com>
2020-12-15 11:29:40 -0500

    first exercise

---
+++ person.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+
+    def greet(self, other_person):
+        print('Hello {}, I am {}!'.format(other_person.name, self.name))
+
+class = Vehicle
+
+sonny = Person("Sonny", "sonny@hotmail.com", "483-485-4948")
+print(sonny.name, sonny.email, sonny.phone)
+
+jordan = Person("Jordan", "jordan@aol.com", "495-586-3456")
+print(jordan.name, jordan.email, jordan.phone)
